2aca287ea42787af5e650081daf094fdf8dc1051
Killercat103 <real.killercat103@gmail.com>
2021-10-25 14:04:49 +0200

    More file cleanup and added site placeholder icon.

--- Bot Dashboard/Controllers/HomeController.cs
+++ Bot Dashboard/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace Bot_Dashboard.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
