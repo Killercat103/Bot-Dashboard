51e3dca682deeaf9338d02e2b8aeb106b3003d47
Killercat103 <real.killercat103@gmail.com>
2021-11-26 01:25:09 +0100

    Added a server menu to show what servers are available to moderate by the user.

--- Bot Dashboard/Controllers/UsersController.cs
+++ Bot Dashboard/Controllers/UsersController.cs
@@ -1,26 +1,59 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Bot_Dashboard.Controllers
 {
-	[Authorize(AuthenticationSchemes = "Discord")]
+
 	public class UsersController : Controller
 	{
+		protected class ClientUser { public string? Access_Token { get; set; } }
+
+		protected class Guild
+		{
+			public long ID { get; set; }
+			public string? Name { get; set; }
+			public string? Icon { get; set; }
+			public bool Owner { get; set; }
+			public long Permissions { get; set; }
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public string Login()
+		public IActionResult Guilds()
 		{
-			string id = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-			return id;
-		}
+			string? discordSession = HttpContext.Session.GetString("Discord.Session");
 
-		[AllowAnonymous]
-		public IActionResult DiscordAuthFailed()
-		{
+			if (string.IsNullOrEmpty(discordSession)) { return RedirectToAction("Login", "OAuth2"); }
+
+			ClientUser? user = JsonConvert.DeserializeObject<ClientUser>(discordSession);
+
+
+			RestClient restClient = new();
+			RestRequest restRequest = new();
+
+			restRequest.AddHeader("Authorization", "Bearer " + user?.Access_Token);
+
+			restClient.BaseUrl = new Uri("https://discord.com/api/v9/users/@me/guilds");
+			IRestResponse? response = restClient.Get(restRequest);
+
+			IList<Guild>? allGuilds = JsonConvert.DeserializeObject<List<Guild>>(response.Content);
+
+			IList<Guild> guilds = new List<Guild>();
+
+
+			for (int i = 0; i < allGuilds?.Count; ++i)
+			{
+				Guild? guild = allGuilds[i];
+
+				if (guild != null && ( guild.Owner == true || (guild.Permissions | 0x20L) == guild.Permissions )) { guilds.Add(guild); }
+			}
+
+			ViewData["Guilds"] = JsonConvert.SerializeObject(guilds, Formatting.Indented);
+
 			return View();
 		}
 	}
