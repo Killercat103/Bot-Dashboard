82a49a89fa526f0962de005b5636baaf79876292
Killercat103 <real.killercat103@gmail.com>
2021-12-07 01:36:05 +0100

    Configured httpresponses for exceptions

--- Bot Dashboard/Controllers/ExceptionController.cs
+++ Bot Dashboard/Controllers/ExceptionController.cs
@@ -4,7 +4,18 @@ namespace Bot_Dashboard.Controllers
 {
 	public class ExceptionController : Controller
 	{
-		public IActionResult URL404(string url)
+		public IActionResult Index(int httpstatus = 500)
+		{
+			return httpstatus switch
+			{
+				401 => RedirectToAction("Login", "OAuth2"),
+				403 => RedirectToAction("Forbidden", "Exception"),
+				404 => RedirectToAction("NotFound", "Exception"),
+				_ => View()
+			};
+		}
+
+		public IActionResult NotFound(string url)
 		{
 			ViewData["URL"] = url;
 			return View();
